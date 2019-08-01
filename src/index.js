609c0f0de6fee3918d67e526a29930eaaf0f693d
jshepp27 <j.sheppard.ldn@gmail.com>
2019-08-01 11:56:47 +0100

    Managing state in Board Component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-§
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };  
-    }
 
-    render() {
+function Square(props) {
         return (
-            <button className="square" onClick={() => alert('click')}>
-                {this.props.value}
+            <button 
+            className="square" 
+            onClick={props.onClick}>
+                {props.value}
             </button>
         );
-    }
 }
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+        }
+    }
+
     renderSquare(i) {
-      return <Square value={i}/>;
-    }   
+      return <Square 
+      value={this.state.squares[i]}
+      onClick={() => this.handelClick(i)}
+      />;
+    }
+    
+    handelClick(i){
+        const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }  
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
+        
+    }
   
     render() {
-      return (
+        // const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');  
+        
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner'+ winner;
+        } else {
+            status = 'Next player' + (this.state.xIsNext ? 'X' : 'O');
+        }
+
+        return (
         <div>
           <div className="board-row">
             {this.renderSquare(0)}
@@ -50,3 +77,22 @@ class Board extends React.Component {
 
 ReactDOM.render(<Board />, document.getElementById('root'));
 
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
